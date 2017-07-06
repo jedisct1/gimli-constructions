25d972c48901f0c7e44c8eec36a67e76b845aec9
Frank Denis <github@pureftpd.org>
2017-07-06 21:41:48 +0200

    Use the capacity for the MAC

--- src/secretbox.c
+++ src/secretbox.c
@@ -145,8 +145,8 @@ gimli_secretbox_encrypt_iv(uint8_t *c, const void *m_, size_t mlen,
     gimli_core_u8(buf);
 
     gimli_secretbox_xor_enc(buf, ct, m, mlen);
-    COMPILER_ASSERT(gimli_secretbox_MACBYTES <= gimli_RATE);
-    mem_cpy(mac, buf, gimli_secretbox_MACBYTES);
+    COMPILER_ASSERT(gimli_secretbox_MACBYTES <= gimli_BLOCKBYTES - gimli_RATE);
+    mem_cpy(mac, buf + gimli_RATE, gimli_secretbox_MACBYTES);
 
     return 0;
 }
@@ -179,8 +179,10 @@ gimli_secretbox_decrypt(void *m_, const uint8_t *c, size_t clen,
     gimli_core_u8(buf);
 
     gimli_secretbox_xor_dec(buf, m, ct, mlen);
-    COMPILER_ASSERT(gimli_secretbox_MACBYTES <= gimli_RATE);
-    cv = mem_ct_cmp_u32(state, pub_mac, gimli_secretbox_MACBYTES / 4);
+    COMPILER_ASSERT(gimli_secretbox_MACBYTES <= gimli_BLOCKBYTES - gimli_RATE);
+    COMPILER_ASSERT(gimli_RATE % 4 == 0);
+    cv = mem_ct_cmp_u32(state + gimli_RATE / 4, pub_mac,
+                        gimli_secretbox_MACBYTES / 4);
     mem_ct_zero_u32(state, gimli_BLOCKBYTES / 4);
     if (cv != 0) {
         mem_zero(m, mlen);
